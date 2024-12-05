d30134de370e637ee25d8b479691ae8c48fcd364
joymacharia <machajoy23@gmail.com>
2024-12-05 21:21:55 +0300

    add config

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 from datetime import timedelta
 import dj_database_url
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -97,8 +98,9 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # }
 
 DATABASES = {
-    "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    "default": dj_database_url.parse(config("DATABASE_URL"))
 }
+    
 
 
 # Password validation
